f3171d926cd6018449f0981efcce3760486b061c
Sebastiao-C <sebastiao.caldas@tecnico.ulisboa.pt>
2024-12-08 21:54:38 +0000

    Update backend URL for production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000", // Your FastAPI backend URL
+    baseURL: "fitsai-eaecdcbqhxbhghaf.westeurope-01.azurewebsites.net", // Your FastAPI backend URL
 });
 
 export default api;
